207526d69537babc943731c73e58d6d4c7d55845
TianTran <87957546+thienzone280596@users.noreply.github.com>
2024-03-31 00:48:12 +0700

    Bai7 cách sử dụng template string

---
+++ Bai7/bai7.js
@@ -0,0 +1,7 @@
+const a = 5;
+const b = 10;
+console.log("Fifteen is " + (a + b) + " and\nnot " + (2 * a + b) + ".");
+console.log('Cách 2 sử dụng template string thay vì cộng chuỗi')
+console.log(`Fifteen is ${a + b} and
+not ${2 * a + b}.`);
+console.log(`in ra gia tri cua a = ${a}, b = ${b}, va a + b = ${a+b}`)
