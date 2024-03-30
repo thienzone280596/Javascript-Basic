7921bc53d14a280ffdeceb5ff23b9c8cf12c19f0
TianTran <87957546+thienzone280596@users.noreply.github.com>
2024-03-31 00:35:21 +0700

    Bai 6 cach su dung number trong javascript

---
+++ Bai6/bai6.js
@@ -0,0 +1,9 @@
+console.log('Hello world from html'.toUpperCase)
+let a = 5
+let b = 6
+let c = 6.9
+let d = '5'
+//cach de conver tu string sang number
+let e =     Number('5')
+console.log('e: ', e,'type: ', typeof e)
+console.log(a/d)
