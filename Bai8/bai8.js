e46461b589814603242c832ac23dbe8052255c4a
TianTran <87957546+thienzone280596@users.noreply.github.com>
2024-03-31 01:05:58 +0700

    Cách khai báo và sử dụng một object

---
+++ Bai8/bai8.js
@@ -0,0 +1,12 @@
+console.log('Bài 8, bài học về object')
+//Khai báo một object rỗng
+let a = {};
+let object = {
+    name: 'Tran Chi Thien',
+    age: 28,
+    job: 'dev'
+}
+console.log(object)
+console.log(`my name is ${object.name}, my age is ${object.age}, my job ${object.job}`)
+//cach de truyen dong mot gia tri trong object 
+console.log(`Truyen dong gia tri trong object ${object['name']}`)
