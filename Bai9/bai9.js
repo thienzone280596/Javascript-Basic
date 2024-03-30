81671c6e41c8dd483f87ba38008626f335b02a04
TianTran <87957546+thienzone280596@users.noreply.github.com>
2024-03-31 01:26:29 +0700

    Cách khởi tạo và sử dụng một aray trong javascript

---
+++ Bai9/bai9.js
@@ -0,0 +1,16 @@
+//Cach su dung array trong javascript
+//Khai bao mot array
+let arrayString = ['a', 'b', 'c']
+console.log(`in ra gia tri cua array: ${arrayString}`)
+//Thay doi gia tri trong mang
+const cars = ["Saab", "Volvo", "BMW"];
+cars[0] = "Opel";
+console.log(`in ra mang xe hoi ${cars}`)
+const fruits = ["Banana", "Orange", "Apple", "Mango"];
+console.log(`Chuyen mang thanh mot chuoi ${fruits.toString()}`)
+//mang doi tuong
+const person = ["John", "Doe", 46];
+console.log(` mang doi tuong ${person}`)
+//Them phan tu vao array 
+fruits.push('banana')
+console.log(`in ra gia tri mang moi ${fruits}`)
